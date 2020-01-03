c208d2489645200eef8fe492a485070eeea9bf59
Ib Havn <iha@via.dk>
2020-01-03 16:52:53 +0100

    Started on application

--- main/main.c
+++ main/main.c
@@ -3,36 +3,90 @@
  */
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
+#include "freertos/portmacro.h"
 #include "freertos/task.h"
 #include "freertos/timers.h"
 #include "freertos/semphr.h"
 #include "esp_system.h"
+#include "esp_types.h"
 #include "esp_spi_flash.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "driver/periph_ctrl.h"
+#include "driver/gpio.h"
+#include "driver/timer.h"
 
 #include "handler_display.h"
-#include "handler_ds18b20.h"
 #include "handler_wifi.h"
-#include "handler_mqtt.h"
-
-#include "ldr_interface.h"
-
-#define pumpSamplePeriod  pdMS_TO_TICKS(1*60*1000) // 1 minute
 
 static const char *TAG = "MAIN";
-static SemaphoreHandle_t pumpSemaphore = NULL;
+static const int _stepperPattern[8][4] = { { 0, 1, 1, 0 }, { 1, 1, 1, 0 }, { 1,
+		1, 0, 0 }, { 1, 1, 0, 1 }, { 1, 0, 0, 1 }, { 1, 0, 1, 1 },
+		{ 0, 0, 1, 1 }, { 0, 1, 1, 1 } };
 
-// Call back to timer that controls how often the pump data is sampled
-static void pumpTimerCallback(TimerHandle_t xTimer) {
-	xSemaphoreGive(pumpSemaphore);
+static void IRAM_ATTR timer_tg0_isr(void *para) {
+	static int _step = 0;
+
+	int timer_idx = (int) para;
+
+	/* Retrieve the interrupt status and the counter value
+	 from the timer that reported the interrupt */
+	uint32_t intr_status = TIMERG0.int_st_timers.val;
+	TIMERG0.hw_timer[timer_idx].update = 1;
+
+	/* Clear the interrupt*/
+	if ((intr_status & BIT(timer_idx)) && timer_idx == TIMER_0) {
+		TIMERG0.int_clr_timers.t0 = 1;
+	}
+	/* After the alarm has been triggered
+	 we need enable it again, so it is triggered the next time */
+	TIMERG0.hw_timer[timer_idx].config.alarm_en = TIMER_ALARM_EN;
+
+	//----- HERE EVERY #uS -----
+	//Toggle a pin so we can verify the timer is working using an oscilloscope
+	//Toggle the pins state
+	gpio_set_level(GPIO_NUM_16, _stepperPattern[_step][0]);
+	gpio_set_level(GPIO_NUM_2, _stepperPattern[_step][1]);
+	gpio_set_level(GPIO_NUM_14, _stepperPattern[_step][2]);
+	gpio_set_level(GPIO_NUM_15, _stepperPattern[_step][3]);
+
+	_step = (_step + 1) % 8;
 }
 
-static void initTimer(void) {
-	TimerHandle_t timerHandle = xTimerCreate("PumpTimer", pumpSamplePeriod,
-	pdTRUE, (void*) 0, pumpTimerCallback);
+static void example_tg0_timer_init(int timer_idx,
+bool auto_reload, double timer_interval_us) {
 
-	xTimerStart(timerHandle, pdMS_TO_TICKS(500));
+	gpio_reset_pin(GPIO_NUM_16);
+	gpio_reset_pin(GPIO_NUM_2);
+	gpio_reset_pin(GPIO_NUM_14);
+	gpio_reset_pin(GPIO_NUM_15);
+
+	gpio_set_direction(GPIO_NUM_16, GPIO_MODE_OUTPUT);
+	gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+	gpio_set_direction(GPIO_NUM_14, GPIO_MODE_OUTPUT);
+	gpio_set_direction(GPIO_NUM_15, GPIO_MODE_OUTPUT);
+
+	/* Select and initialize basic parameters of the timer */
+	timer_config_t config;
+	config.divider = 800;
+	config.counter_dir = TIMER_COUNT_UP;
+	config.counter_en = TIMER_PAUSE;
+	config.alarm_en = TIMER_ALARM_EN;
+	config.intr_type = TIMER_INTR_LEVEL;
+	config.auto_reload = auto_reload;
+	timer_init(TIMER_GROUP_0, timer_idx, &config);
+
+	/* Timer's counter will initially start from value below.
+	 Also, if auto_reload is set, this value will be automatically reload on alarm */
+	timer_set_counter_value(TIMER_GROUP_0, timer_idx, 0x00000000ULL);
+
+	/* Configure the alarm value and the interrupt on alarm. */
+	timer_set_alarm_value(TIMER_GROUP_0, timer_idx, timer_interval_us);
+	timer_enable_intr(TIMER_GROUP_0, timer_idx);
+	timer_isr_register(TIMER_GROUP_0, timer_idx, timer_tg0_isr,
+			(void*) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
+
+	timer_start(TIMER_GROUP_0, timer_idx);
 }
 
 void app_main() {
@@ -51,9 +105,6 @@ void app_main() {
 			(chip_info.features & CHIP_FEATURE_EMB_FLASH) ?
 					"embedded" : "external");
 
-	pumpSemaphore = xSemaphoreCreateBinary();
-	ldrInit(pumpSemaphore);
-
 // Needed by WIFI
 	nvs_flash_init();
 
@@ -62,15 +113,9 @@ void app_main() {
 	displayPowerUp();
 	u8g2_SetFont(&_u8g2, u8g2_font_profont12_mr);
 
-	char _tmp[30];
-
 // Start WIFI task and let it connect to AP
 	wifiInit();
 
-// Start MQTT Client
-	mqttInit();
-
-// Draw battery symbol
 	char ssid[20];
 	wifiGetSSID(ssid);
 	u8g2_DrawStr(&_u8g2, 5 + DISPLAY_WIFI_SYMBOL_WIDTH, 15, ssid);
@@ -78,49 +123,18 @@ void app_main() {
 	displayBatterySymbol(111, 3, 100);
 	displayUpdate();
 
-	ds18b20Init();
-	int noOfTempSensors = ds18b20FindDevices();
-
 	uint8_t rssiPercent = 0;
 
-	char _topic[40];
-
-	float results[DS18B20_MAX_DEVICES];
-	DS18B20_ERROR errors[DS18B20_MAX_DEVICES] = { 0 };
-
-	int _lastPumpState = -1;
-
-	initTimer();
-
+	example_tg0_timer_init(TIMER_0, 1, 100);
+	int io_state = 0;
 	for (;;) {
-		ds18b20GetAllTemperatures(results, errors);
-
-		for (int i = 0; i < noOfTempSensors; i++) {
-			sprintf(_tmp, "T%d:%4.1f", i, results[i]);
-			u8g2_DrawStr(&_u8g2, 0, 30 + (i * 11), _tmp);
-
-			sprintf(_topic, "/pump/temperature/%s/",
-					ds18b20GetDeviceSerialNumber(i));
-			sprintf(_tmp, "%4.1f", results[i]);
-			esp_mqtt_client_publish(client, _topic, _tmp, 0, 0, 0);
-		}
-
-		int _ldr = ldrSeeLight();
-		if (_lastPumpState != _ldr) {
-			sprintf(_topic, "/pump/state/");
-			sprintf(_tmp, "%d", ldrSeeLight());
-			esp_mqtt_client_publish(client, _topic, _tmp, 0, 0, 0);
-
-			sprintf(_tmp, "Pump: %s", (_ldr ? "ON " : "OFF"));
-			u8g2_DrawStr(&_u8g2, 70, 30, _tmp);
-
-			_lastPumpState = _ldr;
-		}
-
 		wifiGetRSSIPercent(&rssiPercent);
 		displayWifiSymbol(0, 2, rssiPercent);
 		displayUpdate();
 
-		xSemaphoreTake(pumpSemaphore, portMAX_DELAY);
+		vTaskDelay(500 / portTICK_PERIOD_MS);
+		io_state ^= 1;
+		//gpio_set_level(GPIO_NUM_2, io_state);
 	}
+
 }
